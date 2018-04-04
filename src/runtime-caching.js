b2b1c4084a7992f576f980dbf164789f5674cac7
mattscrewtop <mferrell@screwtopmedia.com>
2018-04-04 04:35:14 -0500

    add back in cloudcms in runtime cachings

--- src/runtime-caching.js
+++ src/runtime-caching.js
@@ -32,4 +32,52 @@ toolbox.router.get('/(.*)', toolbox.cacheFirst,
 	networkTimeoutSeconds: 3
 });
 
+// The route for any requests from the cdf-cloud origin
+toolbox.router.get('/(.*)', toolbox.cacheFirst, 
+{
+	cache: 
+	{
+		name: 'cdf-cloud',
+		maxEntries: 10,
+		maxAgeSeconds: 86400 // cache for a day
+	},
+
+	origin: /\.cdf\.cloud$/,
+
+	// Set a timeout threshold of 2 seconds
+	networkTimeoutSeconds: 3
+});
+
+// The route for any requests from the cloudcms.com origin
+toolbox.router.get('/(.*)', toolbox.cacheFirst, 
+{
+	cache: 
+	{
+		name: 'cloud-com',
+		maxEntries: 10,
+		maxAgeSeconds: 86400 // cache for a day
+	},
+
+	origin: /\.cloudcms\.com$/,
+
+	// Set a timeout threshold of 2 seconds
+	networkTimeoutSeconds: 3
+});
+
+// The route for any requests from the cloudcms.net origin
+toolbox.router.get('/(.*)', toolbox.cacheFirst, 
+{
+	cache: 
+	{
+		name: 'cloud-net',
+		maxEntries: 10,
+		maxAgeSeconds: 86400 // cache for a day
+	},
+
+	origin: /\.cloudcms\.net$/,
+
+	// Set a timeout threshold of 2 seconds
+	networkTimeoutSeconds: 3
+});
+
 console.log('CACHE-ITICUS 3');
